a60f3bcb3b31c643008edde43dfeb0ff35ffdec3
Morena Electronica <morenaroldi.xd@gmail.com>
2025-12-08 15:52:40 -0300

    Primer commit

---
+++ python/main.py
@@ -0,0 +1,19 @@
+nota = int(input("que clima hace hoy?: "))
+
+if nota >= 23:
+    print("uuuufaaaa, que calooor!")
+
+elif nota >= 18:
+   print("se nota que esta empezando el calorcito")
+
+elif nota >= 10:
+   print("bueno, esta fresco")
+
+elif nota >= 5:
+   print("ay, que fresquito")
+
+elif nota >= 0:
+   print("ufa, hace frio che!")
+
+else:
+   print("pero que estamos, en el polo norte?!")
---
+++ repaso-python/main.py
@@ -0,0 +1,8 @@
+edad = int(input("¿cuantos años tenes?: "))
+
+if edad >= 18:
+    print("sos mayor de edad!")
+
+else:
+    print("sos menor de edad")
+
